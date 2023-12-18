9115eac2725fe798ec05e3d0f0a6f83b35d1ccc5
M-HamzaAdeem <123412144+M-HamzaAdeem@users.noreply.github.com>
2023-12-18 13:15:51 +0500

    Initial Commit

---
+++ Controllers/CustomersController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using VideoRentalProject.Models;
+
+namespace VideoRentalProject.Controllers
+{
+    public class CustomersController : Controller
+    {
+        // GET: Customers
+        public ActionResult Index()
+        {
+            List<Customer> Customers = new List<Customer>()
+            {
+                new Customer() {id = 1, Name = "Ali Arif"},
+                new Customer() {id = 2, Name = "Irfan Ahmed"}
+            };
+           
+            return View(Customers);
+        }
+    }
+}
---
+++ Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(VideoRentalProject.Startup))]
+namespace VideoRentalProject
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
